ef92600359508925d4e652549504d3c4f25d86ad
MozkaGit <mozkaops@gmail.com>
2023-08-05 01:24:28 +0000

    Reorganizing git repo

--- plexupdater.sh
+++ plexupdater.sh
@@ -1,1 +1,1 @@
-/home/freebox/.local/bin/docker-compose pull && /home/freebox/.local/bin/docker-compose up -d --remove-orphans && docker system prune -af && pip3 install docker-compose -U
+/usr/bin/docker compose pull && /usr/bin/docker compose up -d --remove-orphans && docker system prune -af && /usr/bin/python3 -m pip3 install docker-compose -U
