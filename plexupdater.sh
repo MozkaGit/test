d93af8fbce5da823040a563fdcb16bcd82959a64
MozkaGit <mozkaops@gmail.com>
2023-08-28 20:25:10 +0000

    Modify pip binary

--- plexupdater.sh
+++ plexupdater.sh
@@ -1,1 +1,1 @@
-/usr/bin/docker compose pull && /usr/bin/docker compose up -d --remove-orphans && docker system prune -af && /usr/bin/python3 -m pip3 install docker-compose -U
+/usr/bin/docker compose pull && /usr/bin/docker compose up -d --remove-orphans && docker system prune -af && /usr/bin/python3 -m pip install docker-compose -U
